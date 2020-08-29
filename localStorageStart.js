3e4d93c33fb28271ececa66a7330518628133765
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2020-08-29 17:55:45 +0500

    Update localStorageStart.js

--- localStorageStart.js
+++ localStorageStart.js
@@ -537,7 +537,7 @@ function go() {
 let promise =new Promise(function(resolve,reject){
 
 ///////////////////////////////////////////
-firebase.database().ref(`Score/${className}/${subjectName}/`).once('value', function(data){
+firebase.database().ref(`AnonymousUser`).once('value', function(data){
   if(data.val()){
     resolve(data.val());
   }
