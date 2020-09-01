ae4faf4c81c7df666331a3f10183cede47b24d8f
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2020-09-02 00:21:14 +0500

    Update userInfo.js

--- userInfo.js
+++ userInfo.js
@@ -1,97 +1,101 @@
-var database =firebase.database();
+var database = firebase.database();
 
 let user_name = sessionStorage.getItem("name");
 let user_name1 = sessionStorage.getItem("name1");
 
 let user_points = sessionStorage.getItem("points");
 let user_time = sessionStorage.getItem("time");
-let totalQuestions=sessionStorage.getItem("QuestionLength");
+let totalQuestions = sessionStorage.getItem("QuestionLength");
 
 
-user_points=parseInt(user_points);
-user_points=user_points/10;
+user_points = parseInt(user_points);
+user_points = user_points / 10;
 
-    document.querySelector("span.name").innerHTML = user_name;
-    document.querySelector("span.points").innerHTML = `${user_points} OUT OF ${totalQuestions}`;
-    document.querySelector("span.time_taken").innerHTML = `:${user_time} seconds`;
+document.querySelector("span.name").innerHTML = user_name;
+document.querySelector("span.points").innerHTML = `${user_points} OUT OF ${totalQuestions}`;
+document.querySelector("span.time_taken").innerHTML = `:${user_time} seconds`;
 
-    
 
-    let className = sessionStorage.getItem("className");
+
+let className = sessionStorage.getItem("className");
 let subjectName = sessionStorage.getItem("subjectName");
 ////////////////////////////////////////////////////////
-var clas=document.getElementById("clas").innerHTML=className;
-var sub=document.getElementById("sub").innerHTML="Subject: "+subjectName;
+var clas = document.getElementById("clas").innerHTML = className;
+var sub = document.getElementById("sub").innerHTML = "Subject: " + subjectName;
 ////////////////////////////////////////////////////////
 
 
 
 
 
-//This is This is This is This is score sending
-//function sendMesssage()
-//{
-//   var text1=document.getElementById("prima");
-//   var text2;
-// console.log(text1.childNodes)
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date+' '+time;
-  dateTime=dateTime.toString();
+
+////////////////////////////////////////Implementing/////////////////////////////////////////////////////////
+var condition = sessionStorage.getItem("condition");
+window.onload = () => {
+  if (condition == 0) 
+  {
+    //This is This is This is This is score sending
+    //function sendMesssage()
+    //{
+    //   var text1=document.getElementById("prima");
+    //   var text2;
+    // console.log(text1.childNodes)
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    dateTime = dateTime.toString();
+
+    //console.log(question+"\n"+option1+option2+option3+option4+answer+dateTime);
+
+    var key = firebase.database().ref('Score/').push().key;
+
+    var Score = {
+      Name: user_name,
+      Class: className,
+      Subject: subjectName,
+      totalQuestions: totalQuestions,
+      Score: user_points,
+      TimeSpent: user_time,
+      Time: dateTime
+    }
+    firebase.database().ref(`Score/${className}/${subjectName}/`).push(Score);
+  }
+  condition = 1;
+  sessionStorage.setItem("condition", condition);
+}
+////////////////////////////////////////Implementing/////////////////////////////////////////////////////////
 
 
 
 
-//console.log(question+"\n"+option1+option2+option3+option4+answer+dateTime);
-
-
-var key=firebase.database().ref('Score/').push().key;
-var Score={
-    Name:user_name,
-    Class:className,
-    Subject:subjectName,
-    totalQuestions:totalQuestions,
-    Score:user_points,
-    TimeSpent:user_time,
-    Time:dateTime
+function show() {
+  document.getElementById("feedback").style = "display:block;";
 }
 
-firebase.database().ref(`Score/${className}/${subjectName}/`).push(Score);
-
-
-
-
-function show()
-{
-  document.getElementById("feedback").style="display:block;";
-}
-
-function SendComment()
-{
+function SendComment() {
   var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date+' '+time;
-  dateTime=dateTime.toString();
+  var dateTime = date + ' ' + time;
+  dateTime = dateTime.toString();
 
-  var comment=document.getElementById("comments").value;
+  var comment = document.getElementById("comments").value;
 
   console.log(comment);
 
-  var key=firebase.database().ref('Score/').push().key;
-  var FeedBack={
-      Name:user_name,
-      Class:className,
-      Subject:subjectName,
-      Opinion:comment,
-      Time:dateTime
+  var key = firebase.database().ref('Score/').push().key;
+  var FeedBack = {
+    Name: user_name,
+    Class: className,
+    Subject: subjectName,
+    Opinion: comment,
+    Time: dateTime
   }
-  
+
   firebase.database().ref(`FeedBack`).push(FeedBack);
 
-alert("Thanks for your valuable feedback");
-document.getElementById("feedback").style="display:none;";
-location.href="index.html";    
+  alert("Thanks for your valuable feedback");
+  document.getElementById("feedback").style = "display:none;";
 }
 
