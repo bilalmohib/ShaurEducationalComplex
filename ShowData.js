f4485fab16ecbf4a4047c629d9a6a08fc8596236
Muhammad Bilal Mohib-ul-Nabi <bilalmohib7896@gmail.com>
2020-09-03 10:42:46 +0500

    Update ShowData.js

--- ShowData.js
+++ ShowData.js
@@ -6,7 +6,7 @@ var email_id;
 let user_name = sessionStorage.getItem("Name");
 console.log(user_name);
 
-if (user_name == "mohib2156@gmail.com") {
+if (user_name == "mohib2156@gmail.com"||user_name=="ambertahir303@gmail.com") {
   alert(`Welcome ${user_name}`);
   user_name = "";
 }
@@ -45,7 +45,7 @@ function login() {
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
 
-  while (userEmail != "mohib2156@gmail.com") {
+  while (userEmail != "mohib2156@gmail.com"||userEmail!="ambertahir303@gmail.com") {
     logout();
     alert("You are not the right person to access this");
     return;
