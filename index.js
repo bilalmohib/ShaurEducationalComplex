7e41744d2ad669f8166b3af4784a7a242f8129e4
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-03-28 19:58:55 +0500

    New Version released

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 
 
-let promise = new Promise(function (resolve, reject) {
+// let promise = new Promise(function (resolve, reject) {
 
-    ///////////////////////////////////////////
-    firebase.database().ref(`Notification/`).once('value', function (data) {
-      if (data.val()) {
-        resolve(data.val());
-      }
-      else {
-        reject(`The error has occurred`);
-      }
-    })
+//     ///////////////////////////////////////////
+//     firebase.database().ref(`Notification/`).once('value', function (data) {
+//       if (data.val()) {
+//         resolve(data.val());
+//       }
+//       else {
+//         reject(`The error has occurred`);
+//       }
+//     })
   
-  })
+//   })
   
-  promise.then(function (data) {
-    var AllData = data;
+//   promise.then(function (data) {
+//     var AllData = data;
     
-    document.getElementById("notice").innerHTML+=AllData.Notification;
-    document.getElementById("head").innerHTML=AllData.Heading;
-    console.log(AllData.Name)
+//     document.getElementById("notice").innerHTML+=AllData.Notification;
+//     document.getElementById("head").innerHTML=AllData.Heading;
+//     console.log(AllData.Name)
     
 
-  })
-    .catch(function (err) {
-      console.log(err);
-    })
+//   })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
   // Checking for the users to resubmit the quiz  
   
 
